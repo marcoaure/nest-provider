5ca403149d0d8f6d4fb2f4f1042359bf27e30466
Marco Aurélio <marco.anjos@dasa.com.br>
2021-03-22 10:16:32 -0300

    feat: add example service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { AppService, RegisterUserInterface } from "./app.service";
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getUser(): string[] {
+    return this.appService.getUser();
+  }
+
+  @Post()
+  registerUser(@Body() payload: RegisterUserInterface): RegisterUserInterface {
+    return this.appService.registerUser(payload);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+export interface RegisterUserInterface {
+  name: string;
+  age: number;
+}
 
 @Injectable()
 export class AppService {
+  private user = [];
+
   getHello(): string {
-    return 'Hello World!';
+    return "Hello World!";
+  }
+
+  registerUser(payload: RegisterUserInterface): RegisterUserInterface {
+    this.user.push(payload.name);
+    if (payload.age >= 13) {
+      this.addToHighSchool(payload);
+      return payload;
+    }
+    this.addToKindergarten(payload);
+    return payload;
+  }
+
+  getUser() {
+    return this.user;
+  }
+
+  private addToHighSchool(payload) {
+    console.log("adding user to high school " + payload.name);
+  }
+
+  private addToKindergarten(payload) {
+    console.log("adding to kindergarten " + payload.name);
   }
 }
